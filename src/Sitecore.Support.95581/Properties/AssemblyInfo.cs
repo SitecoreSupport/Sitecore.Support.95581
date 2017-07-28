97265f6c38f33cb5d0117d3b55c5a5f912862a32
DK\psk <psk@sitecore.net>
2017-07-28 12:58:05 -0400

    Add assembly description

--- src/Sitecore.Support.95581/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.95581/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.95581")]
 [assembly: AssemblyProduct("Sitecore.Support.95581")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Re-Index Tree command should skip the Analytics search index")]
+
